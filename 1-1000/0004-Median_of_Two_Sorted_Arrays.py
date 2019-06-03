71f245acd50cd137f3840cfac9288db22dc6b07c
Martin Hu <martinhu@cmu.edu>
2019-06-02 23:22:50 -0500

    0004

--- 1-1000/0004-Median_of_Two_Sorted_Arrays.py
+++ 1-1000/0004-Median_of_Two_Sorted_Arrays.py
@@ -20,7 +20,7 @@ The median is (2 + 3)/2 = 2.5
 """
 
 
-class SolutionClassFinal:
+class SolutionClass1:
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :param nums1: list[int]
@@ -37,10 +37,53 @@ class SolutionClassFinal:
             median = nums2[length // 2]
         return median
 # Space: O(1)
-# Time: O(N)
+# Time: O(m+n)
+
+
+class SolutionClassFinal:
+    def findMedianSortedArrays(self, A, B):
+        m, n = len(A), len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imax, half_len = 0, m, (m + n + 1) / 2
+        while imin <= imax:
+            i = (imin + imax) / 2
+            j = half_len - i
+            if i < m and B[j - 1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i - 1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0:
+                    max_of_left = B[j - 1]
+                elif j == 0:
+                    max_of_left = A[i - 1]
+                else:
+                    max_of_left = max(A[i - 1], B[j - 1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
+# Space: O(1)
+# Time: O(log(m+n))
 
 
 if __name__ == "__main__":
     nums1 = [1, 2, 3]
     nums2 = [5, 6, 4]
-    print(SolutionClassFinal().findMedianSortedArrays(nums1, nums2))
+    print(SolutionClass1().findMedianSortedArrays(nums1, nums2))
